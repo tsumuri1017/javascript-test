5ffbfb40604d52e7b256e5570b79aecebd2f8b9b
つむり <tsumuri2008@gmail.com>
2022-04-19 00:33:06 +0900

    Create conditions.js

---
+++ conditions.js
@@ -0,0 +1,9 @@
+data_Text="The journey of Elaina";
+data_Number=1024;
+data_NumberText="1024";
+data_null=null;
+console.log(data_null);//null
+console.log(data_null=='');//true
+console.log(data_Text);//'The journey of Elaina'
+console.log(data_Number)//1024
+console.log(data_Number==data_NumberText)//注意：True。文字列として入力してもTrueになる。
